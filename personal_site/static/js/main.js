c65946d6936f758863a9f963ffb1f603a5d532e9
wikty <xiaowenbin_999@163.com>
2018-04-24 22:57:00 +0800

    add bootstrap components

---
+++ personal_site/static/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  // enable toggle popovers
+  $('.popover-toggle').popover({
+    container: 'body'
+  });
+  // enable dismiss popovers
+  $('.popover-dismiss').popover({
+    trigger: 'focus',
+    container: 'body'
+  });
+})
+
